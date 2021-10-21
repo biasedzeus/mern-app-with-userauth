db0343ab4617e2a37bba5791007007f40ac14bc6
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-21 20:13:23 +0530

    form added with backend integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,61 @@
-import './App.css';
-import React, { useState } from 'react';
-
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
 
-
-  async function onSubmit (event) {
+  async function handleRegister(event) {
     event.preventDefault();
-    console.log("Name :",name)
-    console.log("Email :",email)
-    console.log("PAssword:",pass)
+    const response = await fetch("http://localhost:3300/api/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
 
+      body: JSON.stringify({
+        name,
+        email,
+        pass,
+      }),
+    });
+
+    const data = await response.json();
+    console.log("Responsii", response);
+    console.log(data);
   }
-
 
   return (
     <div className="container">
-      <form >
-        <input type="text"  value={name} onChange = { (e) => setName(e.target.value)}/>
-        <input type="email" value={email} onChange = { (e) => setEmail(e.target.value)} />
-        <input type="password" value={pass} onChange = { (e) => setPass(e.target.value)}/>
-        <button class="submit-button" onClick={onSubmit}>Submit</button>
+      <h1>register</h1>
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          value={pass}
+          placeholder="Password"
+          onChange={(e) => setPass(e.target.value)}
+        />
+        <br />
+        <input type="submit" value="register" />
         <div>
           <p>Name : {name}</p>
           <p>Email : {email}</p>
           <p>Pass : {pass}</p>
-
         </div>
-
-
       </form>
-
     </div>
   );
 }
