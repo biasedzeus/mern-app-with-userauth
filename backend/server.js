30d92ec770d7ade428b687ce1c07dd876037236c
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-21 20:12:12 +0530

    cors added, post method

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3300, () =>{
-    console.log("Listening At Port 3300");
-})
+const cors = require('cors')
+app.use(cors())
+
 
 app.get('/',(res,req) => {
     console.log("Home Visited...!!!")
 
+})
+
+app.get('/hello',(req,res) =>{
+    res.send('hello World')
+})
+
+app.post('/api/user/register',(req,res) =>{
+console.log(req.body)
+res.json({status :'ok'})
+
+})
+
+
+app.listen(3300, () =>{
+    console.log("Listening At Port 3300");
 })
 
