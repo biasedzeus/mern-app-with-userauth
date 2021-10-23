a060ba7aac27ddaba94bb3a15e2a9ed539fc8bd5
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-23 21:22:04 +0530

    Login&Register Routes Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,22 @@
-import "./App.css";
-import React, { useState } from "react";
+import React from 'react'
+import {BrowserRouter as Router , Route,Switch} from 'react-router-dom'
 
-function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+//Components
+import Login from './Components/Login'
+import Register from './Components/Register'
 
-  async function handleRegister(event) {
-    event.preventDefault();
-    const response = await fetch("http://localhost:3300/api/user/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify({
-        name,
-        email,
-        pass,
-      }),
-    });
-
-    const data = await response.json();
-    console.log("Responsii", response);
-    console.log(data);
-  }
-
-  return (
-    <div className="container">
-      <h1>register</h1>
-      <form onSubmit={handleRegister}>
-        <input
-          type="text"
-          value={name}
-          placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="email"
-          value={email}
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          value={pass}
-          placeholder="Password"
-          onChange={(e) => setPass(e.target.value)}
-        />
-        <br />
-        <input type="submit" value="register" />
+const App = () => {
+    return (
         <div>
-          <p>Name : {name}</p>
-          <p>Email : {email}</p>
-          <p>Pass : {pass}</p>
+            <Router>
+
+                <Route exact path = "/login" component ={Login}/>
+                <Route exact path = "/register" component ={Register}/>
+
+            </Router>
+            
         </div>
-      </form>
-    </div>
-  );
+    )
 }
 
 export default App;
