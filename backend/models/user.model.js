0921c5e496338c252779115d25f28d4921698da2
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-23 21:24:30 +0530

    email set to be Unique

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
     name : {type: String, required: true},
-    email : {type:String,required: true},
+    email : {type:String,required: true, unique: true},
     password : {type : String, required: true},
     qoute : {type : String}
 
