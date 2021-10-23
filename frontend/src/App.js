758d79cd536bf571653c0a96d798640043d01fbb
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-23 22:44:32 +0530

    hrefs created for login & register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
+import "./App.css"
 import React from 'react'
 import {BrowserRouter as Router , Route,Switch} from 'react-router-dom'
 
 //Components
 import Login from './Components/Login'
 import Register from './Components/Register'
+import DashBoard from './Components/DashBoard'
+
+
 
 const App = () => {
     return (
-        <div>
+        <div className ="container">
             <Router>
 
                 <Route exact path = "/login" component ={Login}/>
                 <Route exact path = "/register" component ={Register}/>
+                <Route exact path = "/dashboard" component ={DashBoard}/>
+
+                <a href="/login">Login</a>
+                <br/>
+                <a href="/register">Register</a>
+
 
             </Router>
             
